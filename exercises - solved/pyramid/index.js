eb5225c176026d7f9cb0fbdafc507415de1b0829
Parth <gamerboy09pc@gmail.com>
2020-08-21 21:19:42 +0530

    Day 4

--- exercises - solved/pyramid/index.js
+++ exercises - solved/pyramid/index.js
@@ -14,6 +14,62 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    for (let i = 0; i < n; i++){
+        s='';
+        for (let j = 0; j < Math.floor(((2*n - 1)/2))-i; j++){
+            s+=' '
+        }
+        for (let j = Math.floor(((2*n - 1)/2))-i; j < Math.floor(((2*n - 1)/2))+i+1; j++){
+            s+='#'
+        }
+        for (let j = Math.floor(((2*n - 1)/2))+i+1; j < 2*n-1; j++){
+            s+=' '
+        }
+        console.log(s);
+    }
+}
 
 module.exports = pyramid;
+
+
+// function pyramid(n, row = 0, level = '') {
+//     if (row === n) {
+//       return;
+//     }
+  
+//     if (level.length === 2 * n - 1) {
+//       console.log(level);
+//       return pyramid(n, row + 1);
+//     }
+  
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//     let add;
+//     if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//       add = '#';
+//     } else {
+//       add = ' ';
+//     }
+//     pyramid(n, row, level + add);
+//   }
+  
+
+//
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//
+//     console.log(level);
+//   }
+// }
+
--- exercises - solved/vowels/index.js
+++ exercises - solved/vowels/index.js
@@ -7,6 +7,54 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    var charMap = {};
+
+	for (let char of str.toLowerCase()){
+
+		if(!charMap[char]) {
+			charMap[char] = 1;
+		} else {
+			charMap[char]++;
+		}
+
+    }
+    
+    return (charMap['a'] + charMap['e'] + charMap['i'] + 
+    charMap['o'] + charMap['u']) || 0;
+}
 
 module.exports = vowels;
+
+
+// function vowels(str, iter = 0, count = 0) {
+//     if (iter===str.length){
+//         return count;
+//     }
+
+//     if("aeiou".includes(str[iter].toLowerCase())) {
+//         count++;
+//     } 
+    
+//     return vowels(str, iter+1, count);
+// }
+
+
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+//
+//   return count;
+// }
+
+
+// function vowels(str) {
+//     const matches = str.match(/[aeiou]/gi);
+//     return matches ? matches.length : 0;
+// }
