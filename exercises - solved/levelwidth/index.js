8968b31b72cce0b7994216dacef9fae4efae0e0d
Parth <gamerboy09pc@gmail.com>
2020-09-17 15:02:31 +0530

    Day 10

--- exercises - solved/levelwidth/index.js
+++ exercises - solved/levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr1 = [root, 's'];
+    const arr2 = [0];
+
+    while(arr1.length > 1) {
+
+        const node = arr1.shift();
+
+        if(node === 's'){
+            arr2.push(0);
+            arr1.push('s');
+        } else {
+            arr1.push(...node.children);
+            arr2[arr2.length - 1]++;
+        }
+        
+    }
+
+    return arr2;
+}
 
 module.exports = levelWidth;
